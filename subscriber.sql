3b55c127bf66e271672327e0c4e84ec390fd2cf3
Criado Sébastien <dev@letgy.com>
2023-04-17 16:58:08 +0200

    résolution de toutes les issues

--- subscriber.sql
+++ subscriber.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : lun. 13 fév. 2023 à 15:45
+-- Généré le : lun. 17 avr. 2023 à 14:56
 -- Version du serveur : 5.7.39
 -- Version de PHP : 8.2.0
 
@@ -20,51 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `subscriber01`
 --
-
--- --------------------------------------------------------
-
---
--- Structure de la table `interest`
---
-
-CREATE TABLE `interest` (
-  `id` int(11) NOT NULL,
-  `interestLabel` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `interest`
---
-
-INSERT INTO `interest` (`id`, `interestLabel`) VALUES
-(1, 'Peinture'),
-(2, 'Sculpture'),
-(3, 'Photographie'),
-(4, 'Art contemporain'),
-(5, 'Films'),
-(6, 'Art numérique\r\n'),
-(7, 'Installations');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `origins`
---
-
-CREATE TABLE `origins` (
-  `id` int(11) NOT NULL,
-  `origineLabel` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `origins`
---
-
-INSERT INTO `origins` (`id`, `origineLabel`) VALUES
-(1, '---'),
-(2, 'Un ami m’en a parlé'),
-(3, 'Recherche sur Internet'),
-(4, 'Publicité sur un magazine');
 
 -- --------------------------------------------------------
 
@@ -92,69 +47,28 @@ INSERT INTO `subscriber` (`id`, `idOrigin`, `idInterest`, `dateCreate`, `email`,
 (3, NULL, NULL, '2023-02-08 00:00:00', 'sarahlamine@gmail.com', 'Sarah', 'Lamine'),
 (4, NULL, NULL, '2023-02-08 00:00:00', 'mo78@laposte.net', 'Mohamed', 'Ben Salam');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `subscriberInterest`
---
-
-CREATE TABLE `subscriberInterest` (
-  `subscriberId` int(11) NOT NULL,
-  `interestId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 --
 -- Index pour les tables déchargées
 --
-
---
--- Index pour la table `interest`
---
-ALTER TABLE `interest`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `origins`
---
-ALTER TABLE `origins`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `subscriber`
 --
 ALTER TABLE `subscriber`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
   ADD KEY `fk_origin` (`idOrigin`),
   ADD KEY `fk_interest` (`idInterest`);
-
---
--- Index pour la table `subscriberInterest`
---
-ALTER TABLE `subscriberInterest`
-  ADD KEY `fk_interest_id` (`interestId`),
-  ADD KEY `fk_subscriber` (`subscriberId`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT pour la table `interest`
---
-ALTER TABLE `interest`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT pour la table `origins`
---
-ALTER TABLE `origins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
 -- AUTO_INCREMENT pour la table `subscriber`
 --
 ALTER TABLE `subscriber`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Contraintes pour les tables déchargées
@@ -166,13 +80,6 @@ ALTER TABLE `subscriber`
 ALTER TABLE `subscriber`
   ADD CONSTRAINT `fk_interest` FOREIGN KEY (`idInterest`) REFERENCES `interest` (`id`),
   ADD CONSTRAINT `fk_origin` FOREIGN KEY (`idOrigin`) REFERENCES `origins` (`id`);
-
---
--- Contraintes pour la table `subscriberInterest`
---
-ALTER TABLE `subscriberInterest`
-  ADD CONSTRAINT `fk_interest_id` FOREIGN KEY (`interestId`) REFERENCES `interest` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `fk_subscriber` FOREIGN KEY (`subscriberId`) REFERENCES `subscriber` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
